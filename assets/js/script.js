fc952c5d4c9dc39373bb0225d6b4ec41ae150d1b
Kruti Patel <kruti.patel0709@gmail.com>
2021-05-16 21:52:09 -0400

    add clickable button to start the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,29 @@ var responsDiv = document.querySelector("#response");
 var submitButton = document.querySelector("#submit");
 var errMsg = document.querySelector("#errorMessage");
 var finalscoreList = document.querySelector("#allHighScore");
+
+/**  Here is the event listener to start the timer and hide the quiz button*/
+startButton.addEventListener('click', function(event){
+    if (event.target === startButton){
+        wrapperSection.style.display = 'none';
+        setTimer();
+        displayQuestions();
+    }
+    console.log("start quiz button clicked");
+});
+
+/** this is the timer funtion which will start counting as soon as the quiz starts*/
+function setTimer(){
+
+    myInterval = setInterval(function(){
+        myTime--;
+        if(myTime <= 0){
+            console.log(myTime);
+            clearInterval(myInterval);
+            alert('Your time is out.')
+        }
+        timeResult.textContent = "Time:"+" "+ myTime;
+        // var timeReset = timeResult.textContent = "Time:"+" "+ myTime;
+    },1000);
+
+}
