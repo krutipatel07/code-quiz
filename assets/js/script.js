24b56b7ce41819c5118bfbfdcbc18e28d2d07d2c
Kruti Patel <kruti.patel0709@gmail.com>
2021-05-16 21:53:24 -0400

    create question and answer array

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,5 +34,55 @@ function setTimer(){
         timeResult.textContent = "Time:"+" "+ myTime;
         // var timeReset = timeResult.textContent = "Time:"+" "+ myTime;
     },1000);
+}
+function displayQuestions(){
+    var myTitle = questions[i].title;
+    titleElement.textContent = myTitle;
+    var myChoice1 = questions[i].choices[0];
+    var myChoice2 = questions[i].choices[1];
+    var myChoice3 = questions[i].choices[2];
+    var myChoice4 = questions[i].choices[3];
 
+    optionList.innerHTML = '';
+
+    var titleList1 = document.createElement("li");
+    titleList1.setAttribute("class", "questionList");
+    var btn1 = document.createElement("button");
+    btn1.setAttribute("class", "questionBtn");
+    btn1.textContent = "1. " + myChoice1;
+    titleList1.appendChild(btn1);
+    optionList.appendChild(titleList1);
+    quizContainer.appendChild(optionList);
+
+    var titleList2 = document.createElement("li");
+    titleList2.setAttribute("class", "questionList");
+    var btn2 = document.createElement("button");
+    btn2.setAttribute("class", "questionBtn");
+    btn2.textContent = "2. " + myChoice2;
+    titleList2.appendChild(btn2);
+    optionList.appendChild(titleList2);
+    quizContainer.appendChild(optionList);
+
+    var titleList3 = document.createElement("li");
+    titleList3.setAttribute("class", "questionList");
+    var btn3 = document.createElement("button");
+    btn3.setAttribute("class", "questionBtn");
+    btn3.textContent = "3. " + myChoice3;
+    titleList3.appendChild(btn3);
+    optionList.appendChild(titleList3);
+    quizContainer.appendChild(optionList);
+
+    var titleList4 = document.createElement("li");
+    titleList4.setAttribute("class", "questionList");
+    var btn4 = document.createElement("button");
+    btn4.setAttribute("class", "questionBtn");
+    btn4.textContent = "4. " + myChoice4;
+    titleList4.appendChild(btn4);
+    optionList.appendChild(titleList4);
+    quizContainer.appendChild(optionList);
+
+    var allQuestionBtn = document.querySelectorAll(".questionBtn");
+    allQuestionBtn.forEach(function(event){
+        event.addEventListener("click", onclickHandler)
+    });
 }
